cc5d72a2067dc7bdfdba0a1e0158c13b4564ecca
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2023-02-22 09:48:57 -0500

    embellishments

--- vue-school-travel-app/src/App.vue
+++ vue-school-travel-app/src/App.vue
@@ -1,8 +1,14 @@
 
 <template>
-  <TheNavigation/>
-  <div class="container">
-    <router-view></router-view>
+<div>
+    <TheNavigation/>
+    <div class="container">
+      <router-view v-slot="{Component}">
+        <transition name="fade" mode="out-in">
+          <component :is="Component" :key="$route.path"></component>
+        </transition>
+      </router-view>
+    </div>
   </div>
 </template>
 
@@ -13,5 +19,18 @@ export default {
   components: {TheNavigation}
 }
 </script>
+
+<style lang="css">
+  .fade-enter-active,
+  .fade-leave-active{
+    transition: opacity 0.3s;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to{
+    opacity:0;
+  }
+
+</style>
 
 
