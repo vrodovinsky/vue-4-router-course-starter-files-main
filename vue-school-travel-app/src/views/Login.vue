a5333839062850e321e7b31cb725c8cc4b8ecd84
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2023-02-24 08:48:31 -0500

    changed back to options api

--- vue-school-travel-app/src/views/Login.vue
+++ vue-school-travel-app/src/views/Login.vue
@@ -12,21 +12,19 @@
 </template>
 
 <script>
-import {ref} from 'vue'
-import {useRouter, useRoute} from 'vue-router'
-
 export default{
-    setup(){
-        const username = ref('')
-        const password = ref('')
-        const router = useRouter()
-        const route = useRoute()
-        const login = () =>{
-            window.user = username.value
-            const redirectPath = route.query.redirect || '/protected'
-            router.push(redirectPath)
+    data(){
+        return {
+            username: '',
+            password: '',
         }
-        return {username, password, login}
     },
+    methods:{
+        login(){
+            window.user = this.username
+            const redirectPath = this.$route.query.redirect || '/protected'
+            this.$router.push(redirectPath)
+        }
+    }
 }
 </script>
